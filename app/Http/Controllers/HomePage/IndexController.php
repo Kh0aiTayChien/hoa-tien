f1a9c8bc6a44abd0cbb21c1611c46f07ee9e8afe
Chiến Lê <81791505+Kh0aiTayChien@users.noreply.github.com>
2024-05-10 10:12:24 +0700

    Update IndexController.php

--- app/Http/Controllers/HomePage/IndexController.php
+++ app/Http/Controllers/HomePage/IndexController.php
@@ -62,7 +62,7 @@ class IndexController extends Controller
     public function sheet(Request $request)
     {
         Sheets::spreadsheet(config('sheet.spreadsheet_id'));
-        $rows = [null, $request->name, $request->phone, $request->email, $request->detail];
+        $rows = ["", $request->name, $request->phone, $request->email, $request->detail];
 
         $rows = array($rows);
 //        try {
